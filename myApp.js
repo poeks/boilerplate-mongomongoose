c5c26a0bedb6c9800b5e3391722d8d9e5eec3405
Roel <roelwinter11@gmail.com>
2021-07-20 17:30:54 +0200

    Use MongoClient from mongodb

--- myApp.js
+++ myApp.js
@@ -1,7 +1,22 @@
 require('dotenv').config();
-const mongoose = require('mongoose');
+const { MongoClient } = require("mongodb");
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = MongoClient(process.env.MONGO_URI);
+
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    // Establish and verify connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Connected successfully to server");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 let Person;
 
