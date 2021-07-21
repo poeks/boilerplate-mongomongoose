7689e200fd6e4f3e88804057a810863087e28d1d
Roel <roelwinter11@gmail.com>
2021-07-21 16:00:39 +0200

    Add Person model

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-
+const { Schema } = mongoose;
 
 async function run() {
   await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})
@@ -10,7 +10,13 @@ async function run() {
 run().catch(console.dir);
 
 
-let Person;
+const personSchema = Schema({
+  name: {type: String, required: true},
+  age: {type: Number, min: 0},
+  favoriteFoods: [String],
+});
+
+let Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
